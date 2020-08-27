35506b335a0ac682d9696bdce858f53b92d7afb1
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-08-27 16:35:38 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,45 +3,57 @@
 const int bpmlimits[]={70,150};
 const int spo2limits[]={90};
 const int respRatelimits[]={30,95};
-bool bpmIsOk(float bpm)
+bool bpmIsOk(float bpm,int lower,int upper)
 {
-       return (bpm>bpmlimits[0] && bpm<bpmlimits[1]);  
+       bpmlimits[0]=lower;
+       bpmlimits[1]=upper;
+       if(bpm>bpmlimits[0] && bpm<bpmlimits[1]) 
+       {
+       bpmlimits[0]=70;
+       bpmlimits[1]=150;
+       return true;
+       }
+       return false;
 }
-bool spo2IsOk(float spo2)
+bool spo2IsOk(float spo2,int lower)
 {
-       return (spo2>spo2limits[0]);  
+       spo2limits[0]=lower;
+       if(spo2>spo2limits[0])
+       {
+       spo2limits[0]=90;
+              return true;
+       }
+       return false;
+              
 }
-bool respRateIsOk(float respRate)
+bool respRateIsOk(float respRate,int lower, int upper)
 {
-       return (respRate>respRatelimits[0] && respRate<bpmlimits[1]);  
+       respRatelimits[0]=lower;
+       respRatelimits[1]=upper;
+       if(respRate>respRatelimits[0] && respRate<bpmlimits[1])
+       {
+       respRatelimits[0]=30;
+       respRatelimits[1]=95;
+        return true;
+       }
+       return false;
 }
 
-bool bpmOk(float value, int lower, int upper)
-{
-       return ((value>=lower)&& (value<=upper));
-}
-bool spo2Ok(float value, int lower)
-{
-       return (value>=lower);
-}
-bool respRateOk(float value, int lower, int upper)
-{
-       return ((value>=lower)&& (value<=upper));
-}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (bpmIsOk(bpm) && spo2IsOk(spo2) && respRateIsOk(respRate));
+  return (bpmIsOk(bpm,bpmlimits[0],bpmlimits[1]) && spo2IsOk(spo2,spo2limits[0]) && respRateIsOk(respRate,respRatelimits[0];,respRatelimits[1]));
 }
 
 int main() {
-  assert(bpmOk(4, 3, 7) == true);
-  assert(bpmOk(70, 10, 20) == false);
-  assert(bpmOk(5, 10, 20) == false);
-  assert(spo2Ok(100,95) == true);
-  assert(spo2Ok(5,10) == false);
-  assert(spo2Ok(120,100) == true);
-  assert(respRateOk(40, 30, 70) == true);
-  assert(respRateOk(7, 1, 2) == false);
-  assert(respRateOk(50, 100, 120) == false);
+  assert(bpmIsOk(4, 3, 7) == true);
+  assert(bpmIsOk(70, 10, 20) == false);
+  assert(bpmIsOk(5, 10, 20) == false);
+  assert(spo2IsOk(100,95) == true);
+  assert(spo2IsOk(5,10) == false);
+  assert(spo2IsOk(120,100) == true);
+  assert(respRateIsOk(40, 30, 70) == true);
+  assert(respRateIsOk(7, 1, 2) == false);
+  assert(respRateIsOk(50, 100, 120) == false);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
 }
